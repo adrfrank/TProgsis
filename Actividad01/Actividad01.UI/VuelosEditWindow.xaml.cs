a2a5136b30c335fc024a65a2fbcef3d03b004a61
Frank Gonzalez <adrfrank@outlook.com>
2015-02-06 13:27:20 -0600

    --

---
+++ Actividad01/Actividad01.UI/VuelosEditWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Actividad01.Data;
+using Actividad01.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Actividad01.UI
+{
+    /// <summary>
+    /// Interaction logic for VuelosEditWindow.xaml
+    /// </summary>
+    public partial class VuelosEditWindow : Window
+    {
+        Vuelo localEntity;
+
+        public VuelosEditWindow()
+        {
+            InitializeComponent();
+        }
+
+        void ShowEntity() { 
+        
+        }
+
+        public void ShowCreate() {
+            localEntity = new Vuelo();
+            ShowEntity();
+            Show();
+        }
+
+        public void ShowEdit(RepositoryEntity entity) {
+            localEntity = (Vuelo)entity;
+            ShowEntity();
+            Show();
+        }
+
+        // Disclaimer, untested! 
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            e.Cancel = true;  // cancels the window close    
+            this.Hide();      // Programmatically hides the window
+        }
+    }
+}
